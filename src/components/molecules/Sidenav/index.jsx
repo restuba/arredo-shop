ddc9f17a93745cb3fb74ba0aa96d537ab6c1acdd
jempolsamparan@gmail.com <jempolsamparan@gmail.com>
2020-12-30 20:00:03 +0700

    update sidebar component

--- src/components/molecules/Sidenav/index.jsx
+++ src/components/molecules/Sidenav/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { logoBlack } from 'assets';
 import { Link, styled } from 'libraries';
 import { breakpoints } from 'utils';
+import {RiAppsLine as HomeIc, RiShoppingCartLine as CartIc, RiStore2Line as ShopIc, RiHeartLine as FavIc, RiAccountCircleLine as ProfileIc, RiSearchLine as SearchIc, RiBuilding2Line as AboutIc} from 'react-icons/ri';
 
 export const Sidenav = ({onClick, isShow}) => {
   return (
@@ -14,11 +15,30 @@ export const Sidenav = ({onClick, isShow}) => {
       </div>
       <nav className="nav-links">
         <ul>
-          <li><Link to="/" className="link">Home</Link></li>
-          <li><Link to="/shop" className="link">Shop</Link></li>
-          <li><Link to="/product" className="link">Product</Link></li>
-          <li><Link to="/cart" className="link">Cart</Link></li>
-          <li><Link to="/checkout" className="link">Checkout</Link></li>
+          <li>
+            <Link to="/" className="link"><HomeIc/>Home</Link>
+          </li>
+          <li>
+            <Link to="/shop" className="link"><ShopIc/>Shop</Link>
+          </li>
+          <li>
+            <Link to="/about" className="link"><AboutIc/>About</Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link to="#" className="link"><SearchIc/>Search</Link>
+          </li>
+          <li>
+            <Link to="/cart" className="link"><CartIc/>Cart (0)</Link>
+          </li>
+          <li>
+            <Link to="/favorite" className="link"><FavIc/>Favorite (0)</Link>
+          </li>
+          <li>
+            <Link to="/profile" className="link"><ProfileIc/>Profile</Link>
+          </li>
         </ul>
       </nav>
     </SidenavWrap>
@@ -65,7 +85,7 @@ const SidenavWrap = styled.header`
     position: absolute;
     top: 0;
     right: 0;
-    background: red;
+    background: tomato;
     text-align: center;
     align-items: center;
     justify-content: center;
@@ -79,7 +99,7 @@ const SidenavWrap = styled.header`
   }
 
   .nav-brand{
-    margin-bottom: 100px;
+    margin-bottom: 60px;
     display: block;
     img{
       width: 100%;
@@ -87,19 +107,46 @@ const SidenavWrap = styled.header`
     }
   }
 
-  .nav-links li{
-    list-style: none;
-    a{
-      font-size: 14px;
-      text-transform: uppercase;
-      position: relative;
-      z-index: 1;
-      padding: 20px 0;
-      display: block;
-      line-height: 0.9;
-      color: #131212;
-      font-weight: normal;
-      text-decoration:  none;
+  .nav-links {
+    ul{
+      margin-bottom: 2rem;
+      li{
+        list-style: none;
+        a{
+          position: relative;
+          display: flex;
+          align-items: center;
+          font-size: 14px;
+          font-weight: normal;
+          text-transform: uppercase;
+          text-decoration:  none;
+          color: #424242;
+          z-index: 1;
+          padding: 20px 0;
+          transition: 0.3s ease-out;
+          &::after{
+            content: '';
+            transition: 0.3s ease-out;
+            width: 70px;
+            height: 3px;
+            position: absolute;
+            background: transparent;
+            left: -150px;
+          }
+          svg{
+            margin-right: 16px;
+            font-size: 20px;
+          }
+        }
+        &:hover, &:focus{
+          a{color: #000;  
+            &::after{
+              background: tomato;
+              left: -95px;
+            }
+          }
+        }
+      }
     }
   }
 `;
